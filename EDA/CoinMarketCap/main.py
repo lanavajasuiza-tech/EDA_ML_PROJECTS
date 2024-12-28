b538faad94221c3082f73a29363052458f633145
andongob <ndongo@gmail.com>
2024-12-28 22:04:33 +0100

    update time-sesion

---
+++ EDA/CoinMarketCap/main.py
@@ -0,0 +1,178 @@
+"""
+This dataset contains information about cryptocurrency prices, market capitalization, and other metrics. The data is collected from CoinMarketCap (
+https://coinmarketcap.com/), a popular website that tracks cryptocurrency prices.
+
+This dataset can be used to:
+
+Analyze the price trends of different cryptocurrencies.
+Compare the market capitalization of different cryptocurrencies. -
+Look at the circulating supply of different cryptocurrencies.
+Analyze the trading volume of different cryptocurrencies.
+Look at the volatility of different cryptocurrencies.
+Compare the performance of different cryptocurrencies against each other or against a benchmark index.
+Identify correlations between different cryptocurrency prices.
+Use the data to build models to predict future prices or other trends.
+
++Info: https://www.kaggle.com/datasets/harshalhonde/coinmarketcap-cryptocurrency-dataset-2023
+"""
+
+
+import sys
+import os
+from utils.processing import DataLoader
+from utils.analyzer import DataAnalyzer
+
+#---------------- CARGAR DATASET -------------------#
+
+# Detectar dinámicamente el directorio raíz del proyecto
+project_root = os.path.dirname(os.path.abspath(__file__))
+sys.path.append(project_root)
+print("Directorio raíz detectado dinámicamente:", project_root)
+
+
+# Ruta simplificada al dataset
+df_path = os.path.join(project_root, "dataSet")
+print(f"Ruta del dataset: {df_path}")
+df = "currencies_data_2023.csv"
+
+#---------------- CARGAR Y PROCESAR LOS DATOS -------------------#
+try:
+    loader = DataLoader(df_path=df_path, df=df)
+    df = loader.load_data()
+    print("\n--- Dataset cargado correctamente ---")
+except FileNotFoundError as e:
+    print(f"Error al cargar el dataset: {e}")
+    df = None
+except ValueError as e:
+    print(f"Error de valor en el dataset: {e}")
+    df = None
+
+# Si los datos se cargaron, proceder al análisis
+if df is not None:
+    # Instanciar el analizador
+    analyzer = DataAnalyzer(df)
+
+    # Llamadas a los métodos del analizador para verificar si se están cargando o no
+    #print(dir(analyzer))  # Verifica que 'nan_summary' esté listado
+
+    analyzer.overview()
+    analyzer.duplicates_analysis()
+    analyzer.missing_values_analysis() # Tarda entre 7 y 10 minutos, paciencia ...
+    analyzer.data_types_analysis()
+else:
+    print("\n--- No se pudo cargar el dataset. Análisis abortado ---")
+
+
+'''
+El DataSet es muy grande  vamos a reducirlo siguiendo estos criterios:
+0. Convertir los dato tipo fecha para manejar la información y prepararlo para predicciones temporales.
+1. Volumen de mercado (volume24h): Las criptomonedas con mayor actividad.
+2. Capitalización de mercado (marketCap): Las criptomonedas más valiosas.
+3. Antigüedad (dateAdded): Las criptomonedas con más historia.
+4. Ranking (cmcRank): Seleccionar las criptomonedas mejor posicionadas.'''
+
+
+# Filtrar criptomonedas según múltiples criterios
+
+from datetime import datetime
+import pandas as pd
+
+# Convertir dataset_end_date a UTC, de cara a manejar la fecha
+dataset_end_date = pd.Timestamp(datetime(2023, 12, 31), tz="UTC")  # Asegurarse de que sea UTC
+one_year_ago = dataset_end_date - pd.Timedelta(days=365)
+
+min_volume = 1e4  # 1e4 = 10,000. Solo incluir criptomonedas con al menos 10,000 unidades transaccionadas en 24h
+min_market_cap = 1e6  # 1e6 = 1,000,000. Solo incluir criptomonedas con capitalización >= 1,000,000
+top_n_rank = 50  # Considerar las 50 mejores posicionadas según cmcRank
+
+# Asegurarse de que las fechas en la columna 'dateAdded' estén en UTC
+df['dateAdded'] = pd.to_datetime(df['dateAdded'], errors='coerce')  # Convertir a datetime
+df['dateAdded'] = df['dateAdded'].dt.tz_convert("UTC")  # Convertir a UTC si no lo está
+
+# Filtrar el dataset según los criterios definidos
+df_combined_filtered = df[
+    (df['volume24h'] >= min_volume) &  # Criptomonedas con volumen >= 10,000
+    (df['marketCap'] >= min_market_cap) &  # Criptomonedas con capitalización >= 1,000,000
+    (df['dateAdded'] <= one_year_ago) &  # Criptomonedas añadidas hace más de un año
+    (df['cmcRank'] <= top_n_rank)  # Criptomonedas en el Top 50
+]
+
+# Eliminar duplicados
+df_combined_filtered = df_combined_filtered.drop_duplicates()
+
+# Mostrar las dimensiones del dataset reducido
+print("\nDataset reducido basado en múltiples criterios:")
+print(df_combined_filtered.shape)
+print("\nPrimeras filas del dataset reducido:")
+print(df_combined_filtered.head())
+
+# Exportar el dataset reducido a la ruta indicada
+export_path = "EDA/CoinMarketCap/dataSet/currencies_data_2023_reduced.csv"
+df_combined_filtered.to_csv(export_path, index=False)
+print(f"\nDataset reducido exportado correctamente a: {export_path}")
+
+# Importamos el nuevo dataSet para trabajar con él
+df_path = os.path.join(project_root, "dataSet")
+print(f"Ruta del dataset: {df_path}")
+df = "currencies_data_2023_reduced.csv"
+
+#---------------- CARGAR Y PROCESAR LOS DATOS -------------------#
+try:
+    loader = DataLoader(df_path=df_path, df=df)
+    df = loader.load_data()
+    print("\n--- Dataset cargado correctamente ---")
+except FileNotFoundError as e:
+    print(f"Error al cargar el dataset: {e}")
+    df = None
+except ValueError as e:
+    print(f"Error de valor en el dataset: {e}")
+    df = None
+
+# Si los datos se cargaron, proceder al análisis
+if df is not None:
+    # Instanciar el analizador
+    analyzer = DataAnalyzer(df)
+
+    # Llamadas a los métodos del analizador para verificar si se están cargando o no
+    #print(dir(analyzer))  # Verifica que 'nan_summary' esté listado
+
+    analyzer.overview()
+    analyzer.duplicates_analysis()
+    analyzer.missing_values_analysis()
+    analyzer.data_types_analysis()
+    print("Columnas categóricas detectadas:", analyzer.columns_cat)
+    print("Columnas numéricas detectadas:", analyzer.columns_num)
+    #analyzer.nan_summary()
+else:
+    print("\n--- No se pudo cargar el dataset. Análisis abortado ---")
+
+# Rellenamos los NaN en Maxsuply
+
+# Rellenar NaN con 0 (mantiene el conteo de NaN pero es un 0)
+df_combined_filtered['maxSupply'] = df_combined_filtered['maxSupply'].fillna(0)
+print("\nPrimeras filas después de rellenar NaN en maxSupply:")
+print(df_combined_filtered['maxSupply'].head())
+
+
+    #---------------- VISUALIZAR LOS DATOS -------------------#
+
+from utils.visualization import DataVisualizationCoordinator
+
+print("\n--- Visualización Combinada ---")
+
+# Columnas seleccionadas para las visualizaciones
+x_col = "Grocery"      # Columna para el eje X del scatterplot
+y_col = "Milk"         # Columna para el eje Y del scatterplot
+bar_box_col = "Grocery"  # Columna para el barplot y boxplot
+cluster_col = "Channel"  # Columna de clusters
+
+# Instanciar el coordinador de visualizaciones
+viz_coordinator = DataVisualizationCoordinator(df)
+
+# Generar todas las visualizaciones en una sola ventana
+try:
+    viz_coordinator.plot_all(x=x_col, y=y_col, column=bar_box_col, clusters=cluster_col)
+except KeyError as e:
+    print(f"Error en las visualizaciones: {e}")
+    print("Columnas disponibles en el DataFrame:", df.columns.tolist())
+    
---
+++ EDA/CoinMarketCap/utils/analyzer.py
@@ -0,0 +1,127 @@
+import pandas as pd
+
+
+class DataAnalyzer:
+    def __init__(self, df):
+        """
+        Inicializa la clase con un DataFrame.
+        
+        :param df: DataFrame a analizar
+        """
+        self.df = df
+        self.columns_num = []
+        self.columns_cat = []
+        self.columns_num = df.select_dtypes(include=['number']).columns.tolist() # Outliners def
+        self.columns_cat = df.select_dtypes(include=['object']).columns.tolist() # categorical summary
+
+
+    def overview(self):
+        """
+        Muestra una vista general del DataFrame: .describe().T, tipos de datos,
+        nombres de columnas, número de filas y columnas.
+        """
+        print("\n--- Estadísticas descriptivas (transpuesta) ---")
+        print(self.df.describe().T)
+        print("\n--- Títulos de las columnas ---")
+        print(self.df.columns.tolist())
+        print("\n--- Número de columnas ---")
+        print(len(self.df.columns))
+        print("\n--- Número de filas ---")
+        print(len(self.df))
+
+    def duplicates_analysis(self):
+        """
+        Analiza duplicados en filas y columnas del DataFrame.
+        """
+        # Filas duplicadas
+        duplicados_filas = self.df[self.df.index.duplicated(keep=False) & self.df.duplicated(keep=False)]
+        print(f"\n--- Número de filas duplicadas ---: {duplicados_filas.shape[0]}")
+        if not duplicados_filas.empty:
+            print("Filas duplicadas exactas:")
+            print(duplicados_filas)
+        else:
+            print("No se encontraron filas duplicadas exactas.")
+
+        # Columnas duplicadas
+        temp_df = self.df.copy()
+        temp_df.loc["Columna_Titulo"] = temp_df.columns
+        duplicados_columnas = temp_df.T[temp_df.T.duplicated(keep=False)].T
+        print(f"\n--- Número de columnas duplicadas ---: {duplicados_columnas.shape[1]}")
+        if not duplicados_columnas.empty:
+            print("Columnas duplicadas exactas:")
+            print(duplicados_columnas.columns.tolist())
+        else:
+            print("No se encontraron columnas duplicadas exactas.")
+
+    def data_types_analysis(self):
+        """
+        Muestra un resumen de las columnas numéricas y categóricas y las almacena.
+        """
+        self.columns_num = self.df.select_dtypes(include=['number']).columns.tolist()
+        self.columns_cat = self.df.select_dtypes(exclude=['number']).columns.tolist()
+        print("\n--- Columnas categóricas ---")
+        print(self.columns_cat)
+        print("\n--- Columnas numéricas ---")
+        print(self.columns_num)
+
+        table_type = pd.DataFrame({
+            "Column Type": ["Numerical", "Categorical"],
+            "Cuantity": [len(self.columns_num), len(self.columns_cat)],
+            "Column": [self.columns_num, self.columns_cat],
+            "Type": [
+                [self.df[col].dtype for col in self.columns_num],
+                [self.df[col].dtype for col in self.columns_cat]
+            ]
+        })
+        tabla_exploded = table_type.explode(['Column', 'Type']).reset_index(drop=True)
+        print("\n--- Resumen de tipos de columnas ---")
+        print(tabla_exploded)
+
+
+    def missing_values_analysis(self):
+        """
+        Analiza los valores perdidos (NaN) en el DataFrame.
+        """
+        total_nan = self.df.isnull().sum().sum()
+        print(f"\n--- Total de valores NaN ---: {total_nan}")
+        if total_nan > 0:
+            print("Existen valores NaN en la tabla. Es necesario manejarlos.")
+        else:
+            print("La tabla no contiene valores NaN.")
+
+        # Crear tabla con NaN por columna
+        table_type = pd.DataFrame({
+            "Column Type": ["Numerical", "Categorical"],  # Cambiado de "Numéricas" y "Categóricas"
+            "Count": [len(self.columns_num), len(self.columns_cat)],  # Cambiado de "Cantidad"
+            "Columns": [self.columns_num, self.columns_cat],  # Cambiado de "Columnas"
+            "Dtype": [
+                [self.df[col].dtype for col in self.columns_num],
+                [self.df[col].dtype for col in self.columns_cat]
+            ]
+        })
+        tabla_exploded = table_type.explode(['Columns', 'Dtype']).reset_index(drop=True)
+        print("\n--- Column Type Summary ---")
+        print(tabla_exploded)
+
+   # Nuevo método para calcular el porcentaje de valores NaN
+    def nan_summary(self):
+        """
+        Calcula y muestra el porcentaje de valores NaN por columna.
+        """
+        total_rows = len(self.df)
+        nan_values = self.df.isnull().sum()
+        nan_percentage = (nan_values / total_rows) * 100
+
+        # Crear un DataFrame resumen
+        nan_report = pd.DataFrame({
+            'Total NaN': nan_values,
+            'Porcentaje NaN': nan_percentage
+        }).sort_values(by='Porcentaje NaN', ascending=False)
+
+        # Mostrar el reporte si hay valores NaN
+        print("\n--- Resumen de valores NaN por columna ---")
+        if nan_report['Total NaN'].sum() > 0:
+            print(nan_report)
+        else:
+            print("No se encontraron valores NaN en el dataset.")
+        return nan_report
